25be05df7c41b02a5482d6d69cf481bb0fbf3024
jeet <jeetverma2810@gmail.com>
2022-03-24 18:13:16 +0530

    aiavaE

--- python/cisco2.py
+++ python/cisco2.py
@@ -1,2 +1,13 @@
-value = input("Enter a vslue : ")
-print(10/value)
+r1 = int(input("enter the number of rows for first matrix"))
+c1 = int(input("enter the number of column for first matrix"))
+matrix = []
+for i in range(r1):
+    a=[]
+    for j in range(c1):
+        a.append(int(input()))
+    matrix.append(a)
+print("elements of first matrix")
+for i in range(r1):
+    for j in range(c1):
+        print(matrix[i][j])
+    print()
